c2fbad7ff139b1ccdc49ae007b8f6b2190fae9dc
Jacco van Elst <j.c.vanelst@uu.nl>
2021-01-31 20:16:19 +0100

    diffing

--- globals.js
+++ globals.js
@@ -1,25 +1,22 @@
+////////////////
+// GLOBALS
+///////////////
 
 // ACCESS_KEY needs to be used for server setup (data store)
 const ACCESS_KEY = 'zeeekretkeey'; 
 
 // Default behavior of (sub) trial phases.
 const FIXCROSS_DURATION = 1000;
-//const INTERTRIAL_INTERVAL = 1000;
 const PRIME_DURATION = 500;
 const RESPONSE_TIMEOUT_DURATION = 2000; //auto, inluding audio playback duration?
 const FEEDBACK_DURATION = 1000;
 const PRIME_GAP_DURATION = 300;
-const DEFAULT_ITI = 1000;
+const DEFAULT_ITI = 500;
 
-//const TRIAL_GAP_DURATION = 1000; -- ITI?
+
 
 // How many items of the same type may appear in series in case of pseudorandomisation
 const MAX_SUCCEEDING_ITEMS_OF_TYPE = 2;
-
-// Deafult restrictions of minimal browser dimensions
-const MIN_WIDTH = 800;
-const MIN_HEIGHT = 600;
-
 
 // Defaults for buttons
 const OK_BUTTON_TEXT = "OK";
@@ -27,6 +24,10 @@ const YES_BUTTON_TEXT = "Yes";
 const NO_BUTTON_TEXT = "No";
 const TRUE_BUTTON_TEXT = "True";
 const FALSE_BUTTON_TEXT = "False";
+
+// Default restrictions of minimal browser dimensions
+const MIN_WIDTH = 800;
+const MIN_HEIGHT = 600;
 
 // Bail out string for mobiles
 const BAIL_OUT_MOBILE_TEXT = "Please run this experiment on a PC or Laptop.";
